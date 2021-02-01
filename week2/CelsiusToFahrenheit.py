ebda12f138eeb395614e50610025e47b77c5235c
Izardo <isabelladoylegti@gmail.com>
2021-02-01 21:19:32 +0000

    Added comment and an input request

--- week2/CelsiusToFahrenheit.py
+++ week2/CelsiusToFahrenheit.py
@@ -1,4 +1,8 @@
-celsius = 37.5
+# this program converts celsius to fahrenheit
+# author: Isabella Doyle
 
-fahrenheit = (celsius * 1.8) + 32
-print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+celsius = int(input("Enter the temperature in celsius:"))   # asks for input of celsius temperature
+
+fahrenheit = (celsius * 1.8) + 32                           # converts celcius to fahrenheit    
+
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit)) # prints fahrenheit conversion
