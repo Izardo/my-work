5674100ffd398dad976fa0f0ffb9d7172b437296
Izardo <isabelladoylegti@gmail.com>
2021-02-24 13:34:15 +0000

    created new folder for week 5

--- week 5_data-structures/firstTuple.py
+++ week 5_data-structures/firstTuple.py
@@ -1,7 +1,7 @@
 # The first tuple stores the months of the year
 # The second tuple store summer months the contents of 
 # which are printed out one at a time using a for loop
-# Author: Isabella Doyle
+# Author: Isabella Doyle 
 
 # the variable "year" stores 12 months in a tuple
 year = ("January", 
@@ -18,9 +18,9 @@ year = ("January",
                 "December")
 
 # the summer variable contains the summer months indicated
-# at index 4 through to 7 in the year tuple
+# at index 4 through to 7 in the 'year' tuple
 summer = year[4:7]
-# this for loop iterates through the summer months and prints 
-# each out individually
+# uses the for loop to iterate through the summer months 
+# and prints each out individually
 for month in summer:
     print(month)
