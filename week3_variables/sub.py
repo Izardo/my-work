21586ffec680f9ed3a3f360eeddf52f56c527db7
Izardo <isabelladoylegti@gmail.com>
2021-03-07 18:55:11 +0000

    commiting changes

---
+++ week3_variables/sub.py
@@ -0,0 +1,9 @@
+# This program subtracts one number from a second number
+# Author: Isabella Doyle
+
+firstNum = int(input('Enter the first number:'))
+secondNum = int(input('Enter the second number:'))
+
+sum = firstNum - secondNum
+
+print('{} minus {} equals {}'.format(firstNum, secondNum, sum))
---
+++ week6_functions/pop.py
@@ -0,0 +1,9 @@
+# This program creates a list and pops of the item at index 1
+# Author: Isabella
+
+list = [1, 2, 3]
+a, b, c = list
+list.pop(1)
+
+print(list)
+print(a, b, c)
