9e33afa0a6356b8851d7c22f91711af26856a886
Izardo <isabelladoylegti@gmail.com>
2021-03-15 10:58:23 +0000

    changed name of parent folder

--- week3/sub.py
+++
@@ -1,9 +0,0 @@
-# This program subtracts one number from a second number
-# Author: Isabella Doyle
-
-firstNum = int(input('Enter the first number:'))
-secondNum = int(input('Enter the second number:'))
-
-sum = firstNum - secondNum
-
-print('{} minus {} equals {}'.format(firstNum, secondNum, sum))
