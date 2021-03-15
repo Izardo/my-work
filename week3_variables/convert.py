7c7cc25366a297452f8c564328ca8b136bee13a7
Izardo <isabelladoylegti@gmail.com>
2021-03-15 10:05:34 +0000

    edited comments

--- week3_variables/convert.py
+++ week3_variables/convert.py
@@ -1,7 +1,7 @@
 # This program converts dollars to cents
 # Author: Isabella Doyle
 
-amount = round(float(input("Enter amount(USD):")),2)  # requests input of dollar amount
-cents = int(amount * 100)                             # convert dollars to cent
+amount = round(float(input("Enter amount(USD):")),2)    # requests input of dollar amount & rounds to two dec. places
+cents = int(amount * 100)   # converts dollars to integer and asssigns it to the variable cent
 
 print(" {} dollars is {} cents.".format(amount, cents))
