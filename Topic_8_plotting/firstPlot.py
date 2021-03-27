14c91344cddbc9d1df9bd9f6bb68be525420704d
Izardo <isabelladoylegti@gmail.com>
2021-03-27 12:57:04 +0000

    changed variable names

--- Topic_8_plotting/firstPlot.py
+++ Topic_8_plotting/firstPlot.py
@@ -6,9 +6,9 @@ import numpy as np
 
 # creates a numpy array
 x = np.array(range(1, 101))
-y = xPlot * xPlot   # multiplies each item by itself ( y = x(2) )
+y = x * x   # multiplies each item by itself ( y = x(2) )
 
 # specifies the data to display in plot
-plt.plot(xPlot, yPlot)
+plt.plot(x, y)
 # opens new window with the plot
 plt.show()
