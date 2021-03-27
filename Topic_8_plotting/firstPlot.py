e2d0b75dba9ff1eacbf2247bf81b3f0a22acc44f
Izardo <isabelladoylegti@gmail.com>
2021-03-27 08:56:20 +0000

    added plot exercise from week 8 exercises

---
+++ Topic_8_plotting/firstPlot.py
@@ -0,0 +1,14 @@
+# creating my first plot
+# Author: Isabella Doyle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# creates a numpy array
+x = np.array(range(1, 101))
+y = xPlot * xPlot   # multiplies each item by itself ( y = x(2) )
+
+# specifies the data to display in plot
+plt.plot(xPlot, yPlot)
+# opens new window with the plot
+plt.show()
